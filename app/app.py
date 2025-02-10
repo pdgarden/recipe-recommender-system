9ccdeee0de3ec2d7455f4fe37790b288e63e0d62
pdgarden <paul.desjardin1@gmail.com>
2025-02-10 10:19:14 +0100

    refactor(2-good-practices): mv constants to constants.py

--- app/app.py
+++ app/app.py
@@ -5,27 +5,12 @@ Streamlit app implementing a recipe recommender system
 # -------------------------------------------------------------------------------------------------------------------- #
 # Imports
 
-from pathlib import Path
-
 import pandas as pd
 import plotly.express as px
 import streamlit as st
 
+import constants
 from utils.scoring import compute_recipe_affinity_score, labelize_recipes
-
-# -------------------------------------------------------------------------------------------------------------------- #
-# Constants
-
-DEFAULT_NB_ASKED_INGREDIENTS = 500
-NB_RECOMMENDED_RECIPES = 20
-PLOT_DISPLAYED_HOVER_COLUMNS = ["Name", "Ingredients", "Origin", "Link"]
-
-df_features = pd.read_csv(Path("data") / "recipe_db.zip")
-
-INGREDIENTS_PER_RECIPE = df_features["Ingredients"].str.split(", ")
-INGREDIENTS = pd.Series([element for list_ in INGREDIENTS_PER_RECIPE for element in list_]).value_counts()
-DEFAULT_ASKED_INGREDIENTS = INGREDIENTS.head(DEFAULT_NB_ASKED_INGREDIENTS).index.str.title()
-
 
 # -------------------------------------------------------------------------------------------------------------------- #
 # App
@@ -38,10 +23,10 @@ st.sidebar.header("Informations about your taste")
 # ------ #
 # Sidebar
 
-liked_ingredients = st.sidebar.multiselect("Select liked ingredients", DEFAULT_ASKED_INGREDIENTS)
+liked_ingredients = st.sidebar.multiselect("Select liked ingredients", constants.DEFAULT_ASKED_INGREDIENTS)
 disliked_ingredients = st.sidebar.multiselect(
     "Select disliked ingredients",
-    DEFAULT_ASKED_INGREDIENTS,
+    constants.DEFAULT_ASKED_INGREDIENTS,
 )
 
 launch_recipes_recommendation = st.sidebar.button("Get recipe recommendations")
@@ -52,45 +37,41 @@ launch_recipes_recommendation = st.sidebar.button("Get recipe recommendations")
 
 if launch_recipes_recommendation:
     # Compute affinity score
-    df_features["affinity_score_label"] = labelize_recipes(
-        df_features["Ingredients"], liked_ingredients, disliked_ingredients
+    constants.DF_FEATURES["affinity_score_label"] = labelize_recipes(
+        constants.DF_FEATURES["Ingredients"], liked_ingredients, disliked_ingredients
     )
-    df_features["affinity_score_estimated"] = compute_recipe_affinity_score(df_features)
+    constants.DF_FEATURES["affinity_score_estimated"] = compute_recipe_affinity_score(constants.DF_FEATURES)
 
     # Select recipes with most affinity among labelized and estimated recipes
-    df_features_affinity_estimated = (
-        df_features.dropna(subset="affinity_score_estimated")
-        .sort_values("affinity_score_estimated")
-        .tail(int(NB_RECOMMENDED_RECIPES / 2))
+    constants.DF_FEATURES_AFFINITY_ESTIMATED = constants.DF_FEATURES.dropna(
+        subset="affinity_score_estimated"
+    ).sort_values("affinity_score_estimated")
+
+    constants.DF_FEATURES_AFFINITY_LABELED = constants.DF_FEATURES.dropna(subset="affinity_score_label").sort_values(
+        "affinity_score_label"
     )
 
-    df_features_affinity_labeled = (
-        df_features.dropna(subset="affinity_score_label")
-        .sort_values("affinity_score_label")
-        .tail(int(NB_RECOMMENDED_RECIPES / 2))
-    )
-
-    df_recommendation = pd.concat([df_features_affinity_estimated, df_features_affinity_labeled])
-    df_recommendation = df_recommendation[PLOT_DISPLAYED_HOVER_COLUMNS]
+    df_recommendation = pd.concat([constants.DF_FEATURES_AFFINITY_ESTIMATED, constants.DF_FEATURES_AFFINITY_LABELED])
+    df_recommendation = df_recommendation[constants.AFFINITY_PLOT_DISPLAYED_HOVER_COLUMNS]
 
     # Recipes vizualisation
     fig_1 = px.scatter(
-        df_features[df_features.affinity_score_label != 0],
+        constants.DF_FEATURES[constants.DF_FEATURES.affinity_score_label != 0],
         x="tsne_comp_0",
         y="tsne_comp_1",
         color="affinity_score_label",
-        hover_data=PLOT_DISPLAYED_HOVER_COLUMNS,
+        hover_data=constants.AFFINITY_PLOT_DISPLAYED_HOVER_COLUMNS,
         color_continuous_scale=px.colors.diverging.RdBu_r,
         color_continuous_midpoint=0,
         title="Liked ingredients - Labelized score",
     ).update_layout({"plot_bgcolor": "#E0E0E0"})
 
     fig_2 = px.scatter(
-        df_features.dropna(subset="affinity_score_estimated"),
+        constants.DF_FEATURES.dropna(subset="affinity_score_estimated"),
         x="tsne_comp_0",
         y="tsne_comp_1",
         color="affinity_score_estimated",
-        hover_data=PLOT_DISPLAYED_HOVER_COLUMNS,
+        hover_data=constants.AFFINITY_PLOT_DISPLAYED_HOVER_COLUMNS,
         color_continuous_scale=px.colors.diverging.RdBu_r,
         color_continuous_midpoint=0,
         title="Liked ingredients - Estimated score",
---
+++ app/constants.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+import pandas as pd
+
+# Constants for app
+
+DEFAULT_NB_ASKED_INGREDIENTS = 500
+NB_RECOMMENDED_RECIPES = 20
+AFFINITY_PLOT_DISPLAYED_HOVER_COLUMNS = ["Name", "Ingredients", "Origin", "Link"]
+
+DF_FEATURES = pd.read_csv(Path("data") / "recipe_db.zip")
+
+INGREDIENTS_PER_RECIPE = DF_FEATURES["Ingredients"].str.split(", ")
+INGREDIENTS = pd.Series([element for list_ in INGREDIENTS_PER_RECIPE for element in list_]).value_counts()
+DEFAULT_ASKED_INGREDIENTS = INGREDIENTS.head(DEFAULT_NB_ASKED_INGREDIENTS).index.str.title()
