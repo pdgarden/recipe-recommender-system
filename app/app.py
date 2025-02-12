92cecbbcba73bd7bef90829317e0a61dc2d5ad5c
pdgarden <paul.desjardin1@gmail.com>
2025-02-12 15:03:58 +0100

    feat(6-recipe-from-nl): add natural language parsing feature to app

--- app/app.py
+++ app/app.py
@@ -10,6 +10,7 @@ import plotly.express as px
 import streamlit as st
 
 import constants
+from utils.llm import extract_clean_ingredients
 from utils.scoring import compute_recipe_affinity_score, labelize_recipes
 
 # -------------------------------------------------------------------------------------------------------------------- #
@@ -18,17 +19,15 @@ from utils.scoring import compute_recipe_affinity_score, labelize_recipes
 st.set_page_config(layout="wide")
 
 st.header("Recipes recommendation")
-st.sidebar.header("Informations about your taste")
+st.sidebar.header("Information about your taste")
 
-# ------ #
-# Sidebar
+# -------------- #
+# Sidebar - inputs
 
-liked_ingredients = st.sidebar.multiselect("Select liked ingredients", constants.DEFAULT_ASKED_INGREDIENTS)
-disliked_ingredients = st.sidebar.multiselect(
-    "Select disliked ingredients",
-    constants.DEFAULT_ASKED_INGREDIENTS,
+recipe_request = st.sidebar.text_area(
+    label="Your request",
+    placeholder="Could you recommend a recipe? I fancy potatoes, carrots, and peas, but hate onions and garlic",
 )
-
 launch_recipes_recommendation = st.sidebar.button("Get recipe recommendations")
 
 
@@ -36,28 +35,39 @@ launch_recipes_recommendation = st.sidebar.button("Get recipe recommendations")
 # Recipe recommendation
 
 if launch_recipes_recommendation:
+    request_ingredients = extract_clean_ingredients(recipe_request)
+    st.markdown(f"""
+List of ingredients from the request associated with:
+* A positive feeling: {", ".join(request_ingredients.positive_ingredients)}
+* A negative feeling: {", ".join(request_ingredients.negative_ingredients)}
+""")
+
     df_features = constants.DF_FEATURES.copy()
 
     # Compute affinity score
     df_features["affinity_score_label"] = labelize_recipes(
-        df_features["Ingredients"], liked_ingredients, disliked_ingredients
+        df_features["Ingredients"],
+        request_ingredients.positive_ingredients,
+        request_ingredients.negative_ingredients,
     )
     df_features["affinity_score_estimated"] = compute_recipe_affinity_score(df_features)
 
     # Select recipes with most affinity among labelized and estimated recipes
-    df_features_AFFINITY_ESTIMATED = df_features.dropna(
-        subset="affinity_score_estimated"
-    ).sort_values("affinity_score_estimated")
-
-    df_features_AFFINITY_LABELED = df_features.dropna(subset="affinity_score_label").sort_values(
-        "affinity_score_label"
+    df_features_affinity_estimated = df_features.dropna(subset="affinity_score_estimated").sort_values(
+        "affinity_score_estimated"
     )
 
-    df_recommendation = pd.concat([df_features_AFFINITY_ESTIMATED, df_features_AFFINITY_LABELED])
-    df_recommendation = df_recommendation[constants.AFFINITY_PLOT_DISPLAYED_HOVER_COLUMNS]
+    df_features_affinity_labeled = df_features.dropna(subset="affinity_score_label").sort_values("affinity_score_label")
 
-    # Recipes vizualisation
-    fig_1 = px.scatter(
+    df_recommendation = pd.concat(
+        [
+            df_features_affinity_estimated,
+            df_features_affinity_labeled,
+        ]
+    ).drop_duplicates(subset="Name")
+
+    # Figures of scores
+    fig_score_label = px.scatter(
         df_features[df_features.affinity_score_label != 0],
         x="umap_comp_0",
         y="umap_comp_1",
@@ -66,9 +76,11 @@ if launch_recipes_recommendation:
         color_continuous_scale=px.colors.diverging.RdBu_r,
         color_continuous_midpoint=0,
         title="Liked ingredients - Labelized score",
+        width=600,
+        height=600,
     ).update_layout({"plot_bgcolor": "#E0E0E0"})
 
-    fig_2 = px.scatter(
+    fig_score_estimated = px.scatter(
         df_features.dropna(subset="affinity_score_estimated"),
         x="umap_comp_0",
         y="umap_comp_1",
@@ -77,14 +89,35 @@ if launch_recipes_recommendation:
         color_continuous_scale=px.colors.diverging.RdBu_r,
         color_continuous_midpoint=0,
         title="Liked ingredients - Estimated score",
+        width=600,
+        height=600,
     ).update_layout({"plot_bgcolor": "#E0E0E0"})
 
+    # --------- #
     # App layouts
-    tab_table, tab_plots = st.tabs(["🗃 Table vizualisation", "📈 Scatter plot visualization"])
+    tab_rec, tab_table, tab_plots = st.tabs(
+        ["Recommendations", "🗃 Table vizualisation", "📈 Scatter plot visualization"]
+    )
 
-    tab_table.text("Here are some recipes that have been identified as corresponding to your tastes")
-    tab_table.dataframe(df_recommendation)
+    # Recommendations in plain text
+    df_to_md = lambda df: "\n".join(  # noqa: E731
+        ("* **" + df.Name + ":** " + df.Ingredients).str.replace("-", " ").head(5).tolist()
+    )
+    tab_rec.markdown("Here are some recipes that have been identified with corresponding ingredients:")
+    tab_rec.markdown(df_to_md(df_recommendation.sort_values("affinity_score_label", ascending=False)))
+    tab_rec.divider()
+    tab_rec.markdown(
+        "Here are some recipes that don't directly contain the ingredients that you mentioned, but similar ingredients:"
+    )
+    tab_rec.markdown(df_to_md(df_recommendation.sort_values("affinity_score_estimated", ascending=False)))
 
+    # Recommendations in table
+    tab_table.markdown("Table of recommended recipes, sorted by affinity score.")
+    tab_table.dataframe(
+        df_recommendation.sort_values("affinity_score_label")[constants.AFFINITY_PLOT_DISPLAYED_HOVER_COLUMNS]
+    )
+
+    # Recommendations in graph
     tab_plots.text("Here is a low dimension representation of recipes. The color corresponds to the affinity score")
-    tab_plots.plotly_chart(fig_1, use_container_width=True)
-    tab_plots.plotly_chart(fig_2, use_container_width=True)
+    tab_plots.plotly_chart(fig_score_label, use_container_width=True)
+    tab_plots.plotly_chart(fig_score_estimated, use_container_width=True)
